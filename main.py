98236a30f16f1a101702794b640734b062eba290
YanBurton <77856309+YanBurton@users.noreply.github.com>
2022-11-23 11:28:18 -0500

    Testing mongodb connection: success (#1)

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+import pymongo as pymongo
+from flask import Flask, request, jsonify
+#from flask_objectid_converter import ObjectIDConverter
+from pymongo import ReturnDocument
+from pymongo.server_api import ServerApi
+#from Schemas import TemperatureSensorSchema
+from bson import json_util, ObjectId
+#from flask_cors import CORS
+import datetime as dt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# loading private connection information from environment variables
+#from dotenv import load_dotenv
+
+#load_dotenv()
+import os
+
+client = pymongo.MongoClient("mongodb+srv://Admin:admin@finalproject.oqgvx2z.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
+db = client.test
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# This is a test to check connection
+if 'weather' not in db.list_collection_names():
+    db.create_collection("weather",
+                         timeseries={'timeField': 'timestamp', 'metaField': 'sensorId', 'granularity': 'minutes'})
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# def print_hi(name):
+#     print(f'Hi, {name}')
+#
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
