a2268d5d21132f25276b79d1baead0766861c22b
Yan Burton <yanburton03@gmail.com>
2022-12-05 16:37:30 -0400

    Added one aggregation, requires revisiting

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-#import ISODate as ISODate
+# import ISODate as ISODate
 import pymongo as pymongo
-#from flask import Flask, request, jsonify
-#from flask_objectid_converter import ObjectIDConverter
-#from pymongo import ReturnDocument
+# from flask import Flask, request, jsonify
+# from flask_objectid_converter import ObjectIDConverter
+# from pymongo import ReturnDocument
 from pymongo.server_api import ServerApi
-#from Schemas import TemperatureSensorSchema
+# from Schemas import TemperatureSensorSchema
 from bson import json_util, ObjectId
-#from flask_cors import CORS
+# from flask_cors import CORS
 from datetime import datetime, tzinfo, timezone
 # loading private connection information from environment variables
-#from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-#load_dotenv()
+# load_dotenv()
 import os
 
 client = pymongo.MongoClient("mongodb+srv://Admin:admin@finalproject.oqgvx2z.mongodb.net/?retryWrites=true&w=majority",
@@ -26,6 +26,7 @@ if 'Environment' not in db.list_collection_names():
                          timeseries={'timeField': 'timestamp', 'metaField': 'sensorId', 'granularity': 'minutes'})
 
 # Insert dummy data
+"""
 db.Environment.insert_one({
     "timestamp": datetime.now(),
     "sensorId": 450,
@@ -33,12 +34,12 @@ db.Environment.insert_one({
     "humidity": 'humid',
     "brightness": 'bright',
     "sound": 'quiet'
-  })
-
+})
+"""
 db.Environment.aggregate([{
     '$match': {
-         'timestamp': {
-           '$gte': datetime.now()-1,
+        'timestamp': {
+            '$gte': datetime.now() - 1,
             '$lt': datetime.now()
         }
     }
@@ -61,22 +62,43 @@ db.Environment.aggregate([{
 }])
 
 db.Environment.aggregate([{
-        '$match': {
-            'timestamp': {
-                '$gte': datetime.now()-1,
-                '$lt': datetime.now()
-            }
-        }
-    }, {
-        '$group': {
-            '_id': '$humidity',
-            'measurementCount': {
-                '$count': {}
-            }
-        }
-    }, {
-        '$sort': {
-            'humidity': -1
+    '$match': {
+        'timestamp': {
+            '$gte': datetime.now() - 1,
+            '$lt': datetime.now()
         }
     }
+}, {
+    '$group': {
+        '_id': '$humidity',
+        'measurementCount': {
+            '$count': {}
+        }
+    }
+}, {
+    '$sort': {
+        'humidity': -1
+    }
+}
+])
+
+db.Environment.aggregate([{
+    '$match': {
+        'timestamp': {
+            '$gte': datetime.now() - 1,
+            '$lt': datetime.now()
+        }
+    }
+}, {
+    '$group': {
+        '_id': '$sound',
+        'measurementCount': {
+            '$count': {}
+        }
+    }
+}, {
+    '$sort': {
+        'sound': -1
+    }
+}
 ])
